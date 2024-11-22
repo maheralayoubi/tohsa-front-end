ab5eeea679dbafe222fd9244a83c8ab28c9ed65a
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-11-22 23:41:31 +0300

    feat: create sidebar

--- all-pages/home/index.tsx
+++ all-pages/home/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 //sections
 import Hero from "./sections/Hero";
@@ -6,15 +7,26 @@ import SideBar from "./sections/SideBar";
 import News from "./sections/News";
 
 const Index: React.FC = () => {
-    return (
-        <div className="flex items-center justify-between">
-            <SideBar />
-            <div className="container text-center">
-                <Hero />
-            </div>
-            <News />
-        </div>
-    );
+  const [displaySidebar, setDisplaySidebar] = useState<boolean>(true);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) setDisplaySidebar(true);
+      else setDisplaySidebar(false);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-between">
+      <SideBar displaySidebar={displaySidebar} />
+      <div className="container text-center">
+        <Hero />
+      </div>
+      <News />
+    </div>
+  );
 };
 
 export default Index;
--- all-pages/home/sections/SideBar.tsx
+++ all-pages/home/sections/SideBar.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import data from "@/all-pages/home/data/sidebarData.json";
+import Image from "next/image";
 
-const SideBar = () => {
-    return (
-        <div>
-            SideBar
-        </div>
-    )
+interface ISideBarProps {
+  displaySidebar: boolean;
+}
+
+const SideBar = ({ displaySidebar }: ISideBarProps) => {
+  // Only render the sidebar when displayNav is false
+  return displaySidebar === true ? (
+    <div className="sidebar pt-[20px] px-[20px] font-poppins h-[100%]">
+      <ul className="pt-[20px] pl-[20px] space-y-5">
+        {data.map((item, index) => (
+          <li key={index} className="font-bold text-[20px]">
+            <span className="mb-5 block">{item.title}</span>
+            <ul className="w-[151px] space-y-2">
+              {item.items.map((subItem, subIndex) => (
+                <li
+                  key={subIndex}
+                  className="font-normal text-[16px] flex items-center space-x-2 w-[100%] cursor-pointer"
+                >
+                  <Image
+                    src="/images/setting.svg"
+                    alt="setting"
+                    width={32}
+                    height={32}
+                  />
+                  <span>{subItem.name}</span>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : null;
 };
 
 export default SideBar;
