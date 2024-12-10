bc20c8a0a1bca8a584834553fcf1dd2d46097064
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-12-10 14:24:48 +0300

    complate responsive in about page

--- all-pages/about/sections/MissionAbout.tsx
+++ all-pages/about/sections/MissionAbout.tsx
@@ -58,7 +58,7 @@ const TextSection = ({
 );
 
 const MissionSection = () => (
-  <div className="space-y-10">
+  <div className="space-y-5 lg:space-y-10">
     <h2 className="font-bold text-[16px] lg:text-[40px]">Mission & Vision</h2>
     <TextSection title="Mission" content={data.mission} />
     <VisionSection />
@@ -81,31 +81,37 @@ const VisionSection = () => (
 );
 
 const HistorySection = () => (
-  <div className="space-y-10" id="history">
+  <div className="space-y-2 lg:space-y-10" id="history">
     <TextSection title={`History \n Our Journey: The Evolution of HGA-TOHSA`} />
     {/* <h3 className="font-bold text-[24px] lg:text-[32px] leading-[50px]">
       History
       <br />
       Our Journey: The Evolution of HGA-TOHSA
     </h3> */}
-    <div className="flex flex-col lg:flex-row space-y-5 lg:space-y-0 lg:space-x-5">
+    <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-5">
+      {/* Main Image */}
       <Image
         src={data.historyImages.mainImages}
         alt="History Main Image"
         width={200}
         height={200}
-        className="rounded-lg w-full lg:min-w-[67%] lg:min-h-[400px] object-cover"
+        className="rounded-lg w-full lg:min-w-[67%] object-cover"
       />
-      <div className="flex flex-col space-y-5 w-full lg:w-[30%]">
+
+      {/* Side Images */}
+      <div className="flex flex-row lg:flex-col w-full space-x-2 lg:space-x-0 lg:space-y-2">
+        {/* Secondary Image */}
         <Image
           src={data.historyImages.image2}
           alt="History Secondary Image"
           width={280}
           height={195}
-          className="rounded-lg object-contain"
+          className="rounded-lg object-cover w-[50%] lg:w-[280px] lg:max-h-[50%]"
         />
-        <div className="relative w-full rounded-lg overflow-hidden">
-          <div className="absolute z-10 w-full h-full top-0 left-0 flex justify-center items-center flex-col space-y-2">
+
+        {/* "See All" Image with Overlay */}
+        <div className="relative rounded-lg overflow-hidden w-[50%] lg:w-[280px] lg:max-h-[50%]">
+          <div className="absolute inset-0 flex flex-col justify-center items-center z-10 space-y-2">
             <Image
               src="/images/gallery.svg"
               alt="Gallery Icon"
@@ -114,17 +120,18 @@ const HistorySection = () => (
             />
             <p className="text-white text-[16px]">See all 12 images</p>
           </div>
-          <div className="overlay absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+          <div className="absolute inset-0 bg-black opacity-50 rounded-lg"></div>
           <Image
             src={data.historyImages.lastImage}
             alt="History Last Image"
             width={280}
             height={195}
-            className="min-w-[200px] min-h-[195px] w-full"
+            className="object-cover w-full h-full"
           />
         </div>
       </div>
     </div>
+
     <TextSection title="Funding And Early Days" content={data.funding} />
     <TextSection title="Milestones" content={data.milestones} />
     <TextSection title="Current Status" content={data.current} />
@@ -146,18 +153,18 @@ const TeamSection = () => (
       mousewheel
       keyboard
       modules={[Navigation, Mousewheel, Keyboard]}
-      className="mySwiper mt-[40px]"
+      className="mySwiper mt-[8px] lg:mt-[40px]"
     >
       {data.team.teamMembers.map((item) => (
         <SwiperSlide key={item.id}>
-          <div className="bg-[#F0F5FF] p-5 rounded-[20px] font-poppins">
+          <div className="bg-[#F0F5FF] p-2 lg:p-5 rounded-lg lg:rounded-[20px] font-poppins">
             <Image src={item.image} width={352} height={240} alt={item.name} />
-            <div className="mt-5 space-y-2 text-center">
-              <h3 className="font-bold text-[20px] lg:text-[24px]">
+            <div className="mt-2 lg:mt-5 space-y-1 lg:space-y-2 text-center">
+              <h3 className="font-semibold lg:font-bold text-[12px] lg:text-[24px]">
                 {item.name}
               </h3>
-              <p className="text-[14px] lg:text-[16px]">{item.position}</p>
-              <p className="text-[14px] lg:text-[16px] text-[#484848]">
+              <p className="text-[12px] lg:text-[16px]">{item.position}</p>
+              <p className="text-[12px] lg:text-[16px] text-[#484848]">
                 {item.description}
               </p>
             </div>
@@ -183,7 +190,7 @@ const PartnersSection = () => (
       mousewheel
       keyboard
       modules={[Navigation, Mousewheel, Keyboard]}
-      className="mySwiper mt-[40px]"
+      className="mySwiper mt-2 lg:mt-[40px]"
     >
       {[...Array(6)].map((_, index) => (
         <SwiperSlide key={index} className="h-20 relative">
@@ -192,12 +199,12 @@ const PartnersSection = () => (
             alt={`footer ${index + 1}`}
             height={40}
             width={240}
-            className="object-contain self-center max-h-[120px]"
+            className="object-contain self-center max-h-[60px] lg:max-h-[120px] "
           />
         </SwiperSlide>
       ))}
     </Swiper>
-    <div className="w-[100%] h-[300px] lg:h-[460px] mt-[40px]">
+    <div className="w-[100%] h-[300px] lg:h-[460px] mt-2 lg:mt-[40px]">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6014443.780071412!2d142.8754536704671!3d35.45247405148201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60191e54e1f331d7%3A0x1e7ff0811c66662d!2z2KzYp9mF2LnYqSDYs9mI2YPYpw!5e0!3m2!1sar!2s!4v1733423808349!5m2!1sar!2s"
         width="100%"
@@ -216,20 +223,20 @@ const FAQsSection = () => {
   };
 
   return (
-    <div className="space-y-10" id="faqs">
+    <div className="space-y-2 lg:space-y-10" id="faqs">
       <TextSection title="FAQs" />
-      <div className="space-y-5">
+      <div className="space-y-2 lg:space-y-5">
         {data.faqs.map((faq) => (
           <div
             key={faq.id}
-            className={`p-5 rounded-lg border-[1px] ${
+            className={`p-2 lg:p-5 rounded-lg border-[1px] ${
               openFaq === faq.id
                 ? "border-[0px] bg-[#F0F5FF]"
                 : "border-[#BBBBBB]"
             } font-bold text-[20px] lg:text-[24px]`}
           >
             <div className="flex justify-between items-center">
-              <h3>Question {faq.id}</h3>
+              <h3 className="text-[16px]">Question {faq.id}</h3>
               <Image
                 src={
                   openFaq === faq.id
@@ -244,7 +251,9 @@ const FAQsSection = () => {
               />
             </div>
             {openFaq === faq.id && (
-              <p className="mt-5 font-normal">{faq.content}</p>
+              <p className="mt-2 lg:mt-5 text-[12px] font-normal">
+                {faq.content}
+              </p>
             )}
           </div>
         ))}
