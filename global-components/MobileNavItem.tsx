05469a2b24e63763d87318ceba2a7c62f0e08f4c
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-12-15 20:48:53 +0300

    handle close menu when click in item

--- global-components/MobileNavItem.tsx
+++ global-components/MobileNavItem.tsx
@@ -2,44 +2,54 @@ import Link from "next/link";
 import Image from "next/image";
 
 interface IMobileNavItem {
-    href: string,
-    label: string,
+  href: string;
+  label: string;
+  toggle: () => void;
 }
 
-export const MobileNavItem: React.FC<IMobileNavItem> = ({ href, label }) => {
-    return (
-        <li className="cursor-pointer flex justify-between pb-[17px] border-b-[1px] border-[#BBBBBB] text-[14px] font-poppins">
-            <Link href={href} className="flex justify-between w-full">
-                <span>{label}</span>
-                <Image
-                    src="/images/arrow-right.svg"
-                    alt="arrow right"
-                    width={16}
-                    height={16}
-                />
-            </Link>
-        </li>
-    )
+export const MobileNavItem: React.FC<IMobileNavItem> = ({
+  href,
+  label,
+  toggle,
+}) => {
+  return (
+    <li
+      className="cursor-pointer flex justify-between pb-[17px] border-b-[1px] border-[#BBBBBB] text-[14px] font-poppins"
+      onClick={toggle}
+    >
+      <Link href={href} className="flex justify-between w-full">
+        <span>{label}</span>
+        <Image
+          src="/images/arrow-right.svg"
+          alt="arrow right"
+          width={16}
+          height={16}
+        />
+      </Link>
+    </li>
+  );
 };
 
 interface IMobileSubNavItem {
-    href: string,
-    label: string,
+  href: string;
+  label: string;
+  toggle: () => void;
 }
 
-export const MobileSubNavItem: React.FC<IMobileSubNavItem> = ({ href, label }) => {
-    return (
-        <li className="cursor-pointer flex space-x-2 py-[17px] border-b-[1px] border-[#BBBBBB] text-[14px] font-poppins pl-[8px]">
-            <Link href={href} className="flex space-x-2">
-                <Image
-                    src="/images/setting.svg"
-                    alt="setting"
-                    width={24}
-                    height={24}
-                />
-                <span>{label}</span>
-            </Link>
-        </li>
-    )
+export const MobileSubNavItem: React.FC<IMobileSubNavItem> = ({
+  href,
+  label,
+  toggle,
+}) => {
+  return (
+    <li
+      className="cursor-pointer flex space-x-2 py-[17px] border-b-[1px] border-[#BBBBBB] text-[14px] font-poppins pl-[8px]"
+      onClick={toggle}
+    >
+      <Link href={href} className="flex space-x-2">
+        <Image src="/images/setting.svg" alt="setting" width={24} height={24} />
+        <span>{label}</span>
+      </Link>
+    </li>
+  );
 };
-
