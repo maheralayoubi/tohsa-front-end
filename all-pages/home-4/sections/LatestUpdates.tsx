eb88ca666e524ab6dee8b75c7cb6c92dbbe08b26
rezkaudi <audirezk@gmail.com>
2025-01-11 22:29:52 +0300

    fix latest updates section style in home page

--- all-pages/home-4/sections/LatestUpdates.tsx
+++ all-pages/home-4/sections/LatestUpdates.tsx
@@ -21,8 +21,8 @@ import ReadMoreBtn from "@/global-components/ReadMoreBtn";
 const LatestUpdates = () => {
   return (
     <section className="text-base lg:text-base font-normal px-5 py-10 lg:py-32 space-y-10">
-      <div className="w-full flex items-center justify-between lg:px-16">
-        <h2 className="text-[#484848] font-bold lg:text-3xl">Latest Updates</h2>
+      <div className="w-full flex items-center justify-between 2xl:px-16">
+        <h2 className="text-[#57369E] font-bold lg:text-3xl">Latest Updates</h2>
         <ReadMoreBtn content="View All News" href="/news" />
       </div>
 
@@ -31,7 +31,7 @@ const LatestUpdates = () => {
         spaceBetween={20}
         slidesPerView={1}
         breakpoints={{
-          1280: {
+          1536: {
             slidesPerView: 2,
             spaceBetween: 40,
           },
